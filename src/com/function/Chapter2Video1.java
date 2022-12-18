e29418ce9e98e6afe6402c97a7dca116ae51f915
mikhail <mikhail@example.com>
2022-12-18 21:07:28 +0600

    Chapter2Video1 add comment

--- src/com/function/Chapter2Video1.java
+++ src/com/function/Chapter2Video1.java
@@ -12,7 +12,10 @@ public class Chapter2Video1 {
   }
 
   public static void main(String[] args) {
+    // Use functions as classes
+    // Define function (method) as variable:
     Function<Integer, Integer> myTriple = MyMath::triple;
+
     Integer result = myTriple.apply(5);
     System.out.println(result);
   }
