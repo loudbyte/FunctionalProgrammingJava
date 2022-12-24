8cddc3c002744671b54f65203340001aab917e59
mikhail <mikhail@example.com>
2022-12-25 02:12:13 +0600

    Chapter3Challenge2

---
+++ src/com/function/Chapter3Challenge2.java
@@ -0,0 +1,48 @@
+package com.function;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+public class Chapter3Challenge2 {
+    static class Employee {
+        public final String name;
+        public final Integer age;
+        public final String jobTitle;
+        public final Float salary;
+
+        public Employee(String name, Integer age, String jobTitle, Float salary) {
+            this.name = name;
+            this.age = age;
+            this.jobTitle = jobTitle;
+            this.salary = salary;
+        }
+    }
+
+    public static void main(String[] args) {
+        Employee[] employeesArr = {
+                new Employee("John", 34, "developer", 80000f),
+                new Employee("Hannah", 24, "developer", 95000f),
+                new Employee("Bart", 50, "sales executive", 100000f),
+                new Employee("Sophie", 49, "construction worker", 40000f),
+                new Employee("Darren", 38, "writer", 50000f),
+                new Employee("Nancy", 29, "developer", 75000f),
+        };
+        List<Employee> employees = new ArrayList<>(Arrays.asList(employeesArr));
+
+        Map<String, Float> result = employees.stream()
+            .collect(Collectors.groupingBy(e -> e.jobTitle))
+            .entrySet()
+            .stream()
+            .collect(Collectors.toMap(
+                Entry::getKey,
+                entry -> entry.getValue().stream().map(e -> e.salary).reduce(0f, Float::sum)));
+
+        System.out.println(result);
+
+
+    }
+}
